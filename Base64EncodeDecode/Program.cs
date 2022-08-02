1c23ec45791804da59b4b675246bffd6842004a1
fred <domaine75@gmail.com>
2022-08-02 13:06:20 +0200

    Ajoutez des fichiers projet.

---
+++ Base64EncodeDecode/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Base64EncodeDecode
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Action<string> Display = Console.WriteLine;
+      Display("Encode and decode with Base 64");
+      Display(string.Empty);
+
+      string clearText = "A long long time ago in a galaxy far far away";
+      string secret = CryptoUtilities.Base64Encode(clearText);
+      Display("Clear text is:");
+      Display(clearText);
+      Display("Base 64 encoded is:");
+      Display(secret);
+      for (char i = 'a'; i < 'o'; i++)
+      {
+        Display($"Clear text is: {i} and encoded is: {CryptoUtilities.Base64Encode(i.ToString())}");
+        //Display(i.ToString());
+        //Display("Base 64 encoded is:");
+        //Display(CryptoUtilities.Base64Encode(i.ToString()));
+      }
+
+      Display("Press any key to exit:");
+      Console.ReadKey();
+
+    }
+  }
+}
